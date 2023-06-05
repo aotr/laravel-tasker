19a5429667fba82e5269ab309a80cb7a0d0036a1
Animesh Chakraborty <animesh.aotr@gmail.com>
2023-06-05 12:39:17 +0530

    some changes in RunCommand

--- app/Commands/RunCommand.php
+++ app/Commands/RunCommand.php
@@ -2,9 +2,14 @@
 
 namespace App\Commands;
 
-use Illuminate\Console\Scheduling\Schedule;
+use App\Support\TaskManifest;
+use App\Traits\FindsFiles;
+use InvalidArgumentException;
 use LaravelZero\Framework\Commands\Command;
 
+/**
+ * The RunCommand class is responsible for running one or more automated tasks.
+ */
 class RunCommand extends Command
 {
     /**
@@ -12,26 +17,71 @@ class RunCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'run {task* :The name of task}';
+    protected $signature = 'run {task* : The name of the automated task} {--dirty} {--path=* : The paths to scan}';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Run one or more tasks';
+    protected $description = 'Run one or more automated tasks';
 
     /**
-     * Execute the console command.
+     * Handle the command execution.
      *
-     * @return mixed
+     * @return int The exit code (0 for success, non-zero for failure).
      */
-    public function handle()
+    public function handle(): int
     {
-        //
+        foreach ($this->argument('task') as $task) {
+            $result = $this->createTask($this->taskRegistry($task))->perform();
+            if ($result !== 0) {
+                $this->error('Failed to run task: ' . $task);
+                return $result;
+            }
+        }
+
+        return 0;
     }
-    private function taskRe(Type $var = null)
+
+    /**
+     * Create an instance of the specified task and configure it.
+     *
+     * @param string $name The fully qualified name of the task.
+     * @return object The instantiated task object.
+     */
+    private function createTask(string $name): object
     {
-        # code...
+        $task = new $name;
+
+        if (in_array(FindsFiles::class, class_uses_recursive($task))) {
+            if ($this->option('path')) {
+                $task->setFiles($this->option('path'));
+            }
+
+            if ($this->option('dirty')) {
+                $task->setDirty(true);
+            }
+        }
+
+        return $task;
+    }
+
+    /**
+     * Get the class name of the specified task from the task registry.
+     *
+     * @param string $task The name of the task.
+     * @return string The fully qualified class name of the task.
+     * @throws InvalidArgumentException if the task is not registered.
+     */
+    private function taskRegistry(string $task): string
+    {
+        $tasks = resolve(TaskManifest::class)->list();
+
+        if (!isset($tasks[$task])) {
+            throw new InvalidArgumentException('Task not registered: ' . $task);
+        }
+
+        return $tasks[$task];
     }
 }
